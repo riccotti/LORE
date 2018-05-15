37110cb3cfa6d9b8615b55b143ab1845d29a06a3
riccotti <ric.guidotti@gmail.com>
2018-05-15 12:28:12 +0200

    updates exp

--- experiment_lore_vs_anchor.py
+++ experiment_lore_vs_anchor.py
@@ -6,6 +6,7 @@ from prepare_dataset import *
 from neighbor_generator import *
 
 from anchor import anchor_tabular
+from statistics import mode
 
 from sklearn.metrics import f1_score
 from sklearn.metrics import accuracy_score
@@ -95,6 +96,15 @@ def run_experiment(blackbox, X2E, y2E, idx_record2explain, dataset, anchor_expla
 
     dfX2E = build_df2explain(blackbox, X2E, dataset).to_dict('records')
 
+    individual_hit_lore = 0
+    fidelity_acc_lore = fidelity_f1_lore = coverage_lore = coverage_Z_lore = 0
+    precision_lore = [0]
+    individual_hit_anchor = fidelity_acc_anchor = fidelity_f1_anchor = coverage_anchor = coverage_Z_anchor = 0
+    precision_anchor = [0]
+
+    def eval(x, y):
+        return 1 if x == y else 0
+
     print(datetime.datetime.now(), '\tLORE')
     attempt = 0
     while True:
@@ -106,7 +116,8 @@ def run_experiment(blackbox, X2E, y2E, idx_record2explain, dataset, anchor_expla
                                                        returns_infos=True, path=path_data, sep=';', log=verbose)
 
             cc_outcome_lore = lore_explanation[0][0][dataset['class_name']]
-            # print(cc_outcome_lore, type(cc_outcome_lore))
+            # print(cc_outcome_lore, type(cc_outcome_lore), bb_outcome, type(bb_outcome))
+            # print(cc_outcome_lore == bb_outcome)
             individual_hit_lore = hit_outcome(bb_outcome, cc_outcome_lore)
 
             y_pred_bb_lore = lore_info['y_pred_bb']
@@ -118,7 +129,7 @@ def run_experiment(blackbox, X2E, y2E, idx_record2explain, dataset, anchor_expla
             # print(lrule)
             covered_lore = lore.get_covered(lrule, dfX2E, dataset)
             coverage_lore = len(covered_lore) / len(dfX2E)
-            precision_lore = [1 - np.abs(v - cc_outcome_lore) for v in y2E[covered_lore]]
+            precision_lore = [1 - eval(v, cc_outcome_lore) for v in y2E[covered_lore]]
             covered_Z_lore = lore.get_covered(lrule, lore_info['dfZ'].to_dict('records'), dataset)
             coverage_Z_lore = len(covered_Z_lore) / len(lore_info['dfZ'])
             # print(coverage_lore)
@@ -135,6 +146,7 @@ def run_experiment(blackbox, X2E, y2E, idx_record2explain, dataset, anchor_expla
             break
 
         attempt += 1
+
 
     print(datetime.datetime.now(), '\tAnchor')
     attempt = 0
@@ -156,13 +168,16 @@ def run_experiment(blackbox, X2E, y2E, idx_record2explain, dataset, anchor_expla
             covered_anchor = lore.get_covered(arule, dfX2E, dataset)
             coverage_anchor = len(covered_anchor) / len(dfX2E)
             if len(covered_anchor) > 0:
-                cc_outcome_anchor = int(np.round(y2E[covered_anchor].mean()))
+                if isinstance(y2E[0], str):
+                    cc_outcome_anchor = mode(y2E[covered_anchor])
+                else:
+                    cc_outcome_anchor = int(np.round(y2E[covered_anchor].mean()))
             else:
                 cc_outcome_anchor = bb_outcome
 
             # print(cc_outcome_anchor, type(cc_outcome_anchor))
             individual_hit_anchor = hit_outcome(bb_outcome, cc_outcome_anchor)
-            precision_anchor = [1 - np.abs(v - cc_outcome_anchor) for v in y2E[covered_anchor]]
+            precision_anchor = [1 - eval(v, cc_outcome_anchor) for v in y2E[covered_anchor]]
 
             dfZanchor = build_df2explain(blackbox, Zanchor, dataset).to_dict('records')[:1000]
             covered_Z_anchor = lore.get_covered(arule, dfZanchor, dataset)
@@ -187,9 +202,9 @@ def run_experiment(blackbox, X2E, y2E, idx_record2explain, dataset, anchor_expla
 
 def main():
 
-    start_index = 20
+    start_index = -1
 
-    path = '/Users/riccardo/Documents/PhD/OpenTheBlackBox/code/LORE/'
+    path = './'
     path_data = path + 'datasets/'
     path_exp = path + 'experiments/'
 
@@ -206,14 +221,17 @@ def main():
     }
 
     blackbox_list = {
-        # 'svm': LinearSVC,
+        'svm': LinearSVC,
         # 'dt': DecisionTreeClassifier,
         # 'nn': MLPClassifier,
-        'rf': RandomForestClassifier,
+        # 'rf': RandomForestClassifier,
         # 'lr': LogisticRegression,
     }
 
-    experiments_results = open(path_exp + 'lore_vs_anchor_coverage_precision.csv', 'a')
+    d = list(datsets_list.keys())[0]
+    b = list(blackbox_list.keys())[0]
+
+    experiments_results = open(path_exp + 'lore_vs_anchor_coverage_precision_%s_%s.csv' % (d, b), 'a')
 
     for dataset_kw in datsets_list:
         dataset_name, prepare_dataset_fn = datsets_list[dataset_kw]
@@ -234,6 +252,7 @@ def main():
 
             blackbox.fit(X_train, y_train)
             y2E = blackbox.predict(X2E)
+            y2E = np.asarray([dataset['label_encoder'][dataset['class_name']].classes_[i] for i in y2E])
 
             for idx_record2explain in range(len(X2E)):
                 if idx_record2explain <= start_index:
--- test_lore.py
+++ test_lore.py
@@ -11,9 +11,17 @@ warnings.filterwarnings("ignore")
 
 def main():
 
-    dataset_name = 'german_credit.csv'
+    # dataset_name = 'german_credit.csv'
     path_data = '/home/riccardo/Documenti/PhD/OpenTheBlackBox/LORE/datasets/'
-    dataset = prepare_german_dataset(dataset_name, path_data)
+    # dataset = prepare_german_dataset(dataset_name, path_data)
+
+    dataset_name = 'compas-scores-two-years.csv'
+    dataset = prepare_compass_dataset(dataset_name, path_data)
+    print(dataset['label_encoder'][dataset['class_name']].classes_)
+    print(dataset['possible_outcomes'])
+
+    # dataset_name = 'adult.csv'
+    # dataset = prepare_adult_dataset(dataset_name, path_data)
 
     X, y = dataset['X'], dataset['y']
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
@@ -22,6 +30,9 @@ def main():
     blackbox.fit(X_train, y_train)
 
     X2E = X_test
+    y2E = blackbox.predict(X2E)
+    y2E = np.asarray([dataset['possible_outcomes'][i] for i in y2E])
+
     idx_record2explain = 0
 
     explanation, infos = lore.explain(idx_record2explain, X2E, dataset, blackbox,
@@ -44,8 +55,12 @@ def main():
     print(len(covered))
     print(covered)
 
-    y2E = blackbox.predict(X2E)
-    precision = [1-np.abs(v-explanation[0][0][dataset['class_name']]) for v in y2E[covered]]
+    print(explanation[0][0][dataset['class_name']], '<<<<')
+
+    def eval(x, y):
+        return 1 if x == y else 0
+
+    precision = [1-eval(v, explanation[0][0][dataset['class_name']]) for v in y2E[covered]]
     print(precision)
     print(np.mean(precision), np.std(precision))
 
